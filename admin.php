00e4f03d5936c455d461122974c83f9d32ed1ad9
rosto-infinity <rostoinfocus@gmail.com>
2025-07-24 15:28:25 +0100

    slug updated

--- admin.php
+++ admin.php
@@ -13,14 +13,84 @@ function clean_input($data) {
     return htmlspecialchars(stripslashes(trim($data)));
 }
 
+function createSlug($title) {
+    $slug =removeAccents($title);
+    $slug= strtolower( str_replace(' ', '-',$slug));
+    $slug = preg_replace('/[^a-z0-9-]/', '', $slug); // Supprimer les caractères non alphanumériques
+    $slug = preg_replace('/-+/', '-', $slug); // Remplacer les tirets multiples par un seul
+
+    return trim($slug, '-'); // Supprimer les tirets en début et fin de chaîne
+}
+
+function removeAccents($string)
+{
+  $accents = [
+    'à' => 'a',
+    'á' => 'a',
+    'â' => 'a',
+    'ã' => 'a',
+    'ä' => 'a',
+    'å' => 'a',
+    'ç' => 'c',
+    'è' => 'e',
+    'é' => 'e',
+    'ê' => 'e',
+    'ë' => 'e',
+    'ì' => 'i',
+    'í' => 'i',
+    'î' => 'i',
+    'ï' => 'i',
+    'ñ' => 'n',
+    'ò' => 'o',
+    'ó' => 'o',
+    'ô' => 'o',
+    'õ' => 'o',
+    'ö' => 'o',
+    'ø' => 'o',
+    'ù' => 'u',
+    'ú' => 'u',
+    'û' => 'u',
+    'ü' => 'u',
+    'ý' => 'y',
+    'ÿ' => 'y',
+    'À' => 'A',
+    'Á' => 'A',
+    'Â' => 'A',
+    'Ã' => 'A',
+    'Ä' => 'A',
+    'Å' => 'A',
+    'Ç' => 'C',
+    'È' => 'E',
+    'É' => 'E',
+    'Ê' => 'E',
+    'Ë' => 'E',
+    'Ì' => 'I',
+    'Í' => 'I',
+    'Î' => 'I',
+    'Ï' => 'I',
+    'Ñ' => 'N',
+    'Ò' => 'O',
+    'Ó' => 'O',
+    'Ô' => 'O',
+    'Õ' => 'O',
+    'Ö' => 'O',
+    'Ø' => 'O',
+    'Ù' => 'U',
+    'Ú' => 'U',
+    'Û' => 'U',
+    'Ü' => 'U',
+    'Ý' => 'Y'
+  ];
+  return strtr($string, $accents);
+}
 
 if(isset($_POST['add-article'])) {
 
-    // echo "Form submitted";
-    // die();
-
     $title = clean_input($_POST['title']);
-    $slug = $title;
+    $slug =createSlug($title);
+        echo $slug;
+        die();
+
     $introduction = clean_input($_POST['introduction']);
     $content = clean_input($_POST['content']);
 
