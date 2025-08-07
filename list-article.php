9fb2bfe7144195c6453558d9f9c9b4207a4285cc
rosto-infinity <rostoinfocus@gmail.com>
2025-08-07 17:42:12 +0100

    -Recupere la view(layout) de la page d'accueil

--- list-article.php
+++ list-article.php
@@ -19,7 +19,7 @@ $pageTitle = "Page admin list Article";
 // Debut de tampon de la page de sortie
 ob_start();
 
-// Recupere la view(layout) de la page d'accueil
+// -Recupere la view(layout) de la page d'accueil
 require_once 'resources/views/admin/articles/list-article_html.php';
 
 // Récupérer le contenu du tampon de la page d'accueil
