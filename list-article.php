162bd254a4070b386fe02974c37d0e916ee971eb
rosto-infinity <rostoinfocus@gmail.com>
2025-08-07 17:42:24 +0100

    -Récupérer le contenu du tampon de la page d'accueil

--- list-article.php
+++ list-article.php
@@ -22,7 +22,7 @@ ob_start();
 // -Recupere la view(layout) de la page d'accueil
 require_once 'resources/views/admin/articles/list-article_html.php';
 
-// Récupérer le contenu du tampon de la page d'accueil
+// -Récupérer le contenu du tampon de la page d'accueil
 $pageContent = ob_get_clean();
 
 require_once 'resources/views/layouts/admin-layout/layout_html.php';
